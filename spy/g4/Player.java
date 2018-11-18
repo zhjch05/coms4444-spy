e71bbca0c9dffd285d0679728b0b98616bce02dc
Shandukani Mulaudzi <shandu.mulaudzi@gmail.com>
2018-11-18 15:35:42 -0500

    Initialize g4 folder and player

---
+++ spy/g4/Player.java
@@ -0,0 +1,117 @@
+package spy.g4;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
+
+import spy.sim.Point;
+import spy.sim.Record;
+import spy.sim.CellStatus;
+import spy.sim.Simulator;
+import spy.sim.Observation;
+
+public class Player implements spy.sim.Player {
+    
+    private ArrayList<ArrayList<Record>> records;
+    private int id;
+    private Point loc;
+    
+    public void init(int n, int id, int t, Point startingPos, List<Point> waterCells, boolean isSpy)
+    {
+        this.id = id;
+        this.records = new ArrayList<ArrayList<Record>>();
+        for (int i = 0; i < 100; i++)
+        {
+            ArrayList<Record> row = new ArrayList<Record>();
+            for (int j = 0; j < 100; j++)
+            {
+                row.add(null);
+            }
+            this.records.add(row);
+        }
+    }
+    
+    public void observe(Point loc, HashMap<Point, CellStatus> statuses)
+    {
+        this.loc = loc;
+
+        for (Map.Entry<Point, CellStatus> entry : statuses.entrySet())
+        {
+            Point p = entry.getKey();
+            CellStatus status = entry.getValue();
+            Record record = records.get(p.x).get(p.y);
+            if (record == null || record.getC() != status.getC() || record.getPT() != status.getPT())
+            {
+                ArrayList<Observation> observations = new ArrayList<Observation>();
+                record = new Record(p, status.getC(), status.getPT(), observations);
+                records.get(p.x).set(p.y, record);
+            }
+            record.getObservations().add(new Observation(this.id, Simulator.getElapsedT()));
+        }
+    }
+    
+    public List<Record> sendRecords(int id)
+    {
+        ArrayList<Record> toSend = new ArrayList<Record>();
+        for (ArrayList<Record> row : records)
+        {
+            for (Record record : row)
+            {
+                if (record != null)
+                {
+                    toSend.add(record);
+                }
+            }
+        }
+        return toSend;
+    }
+    
+    public void receiveRecords(int id, List<Record> records)
+    {
+        
+    }
+    
+    public List<Point> proposePath()
+    {
+        return null;
+    }
+    
+    /*public List<Integer> getVotes(HashMap<Integer, List<Point>> paths)
+    {
+        for (Map.Entry<Integer, List<Point>> entry : paths.entrySet())
+        {
+            ArrayList<Integer> toReturn = new ArrayList<Integer>();
+            toReturn.add(entry.getKey());
+            return entry.getKey();
+        }
+        return null;
+    } ^THIS HAS AN ERROR^ */
+
+    public List<Integer> getVotes(HashMap<Integer, List<Point>> paths)
+    {
+        for (Map.Entry<Integer, List<Point>> entry : paths.entrySet())
+        {
+            ArrayList<Integer> toReturn = new ArrayList<Integer>();
+            toReturn.add(entry.getKey());
+            return new ArrayList<Integer>(entry.getKey());
+        }
+        return null;
+    }
+    
+    public void receiveResults(HashMap<Integer, Integer> results)
+    {
+        
+    }
+    
+    public Point getMove()
+    {
+        Random rand = new Random();
+        int x = rand.nextInt(2) * 2 - 1;
+        int y = rand.nextInt(2 + Math.abs(x)) * (2 - Math.abs(x)) - 1;
+        return new Point(x, y);
+    }
+}
