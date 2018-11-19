d45527257745e578bab288bf6ddd8144f8b0145a
Shandukani Mulaudzi <shandu.mulaudzi@gmail.com>
2018-11-18 23:34:03 -0500

    Add basic player functionality

--- spy/g4/Player.java
+++ spy/g4/Player.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Arrays;
+import java.util.HashSet;
 
 import spy.sim.Point;
 import spy.sim.Record;
@@ -21,15 +22,18 @@ public class Player implements spy.sim.Player {
     private int id;
     private Point loc;
 
-    private int spy; // player who we think is the spy
-    private HashMap<Integer, Integer> possibleSpies; // all players who could potentially be the spy
-    private HashMap<Point, ArrayList<Integer>> trueRecords;
+    private boolean isSpy;
+
+    private int spy = -1; // player who we think is the spy
+    private HashMap<Integer, HashSet<Point>> possibleSpies; // all players who could potentially be the spy
+    private HashMap<Integer, Integer> suspicionScore;
+    private HashMap<Point, Record> trueRecords;
     
     public void init(int n, int id, int t, Point startingPos, List<Point> waterCells, boolean isSpy)
     {
-        System.out.println("Player g4 number " + id);
-        System.out.println("Am I spy? " + isSpy);
-        System.out.println("Starting at: " + startingPos);
+        //System.out.println("Player g4 number " + id);
+        //System.out.println("Am I spy? " + isSpy);
+        //System.out.println("Starting at: " + startingPos);
 
         this.id = id;
         this.records = new ArrayList<ArrayList<Record>>();
@@ -43,7 +47,10 @@ public class Player implements spy.sim.Player {
             this.records.add(row);
         }
 
-        trueRecords = new HashMap<Point, ArrayList<Integer>>();
+        this.isSpy = isSpy;
+        trueRecords = new HashMap<Point, Record>();
+        possibleSpies = new HashMap<Integer, HashSet<Point>>();
+        suspicionScore = new HashMap<Integer, Integer>();
     }
     
     public void observe(Point loc, HashMap<Point, CellStatus> statuses)
@@ -60,35 +67,94 @@ public class Player implements spy.sim.Player {
                 ArrayList<Observation> observations = new ArrayList<Observation>();
                 record = new Record(p, status.getC(), status.getPT(), observations);
                 records.get(p.x).set(p.y, record);
-                trueRecords.put(p, new ArrayList<Integer>(Arrays.asList(status.getC(), status.getPT())));
-                System.out.println("observed a record at " + p);
+                trueRecords.put(p, record);
+                //System.out.println("observed a record at " + p);
             }
             record.getObservations().add(new Observation(this.id, Simulator.getElapsedT()));
         }
+        /*for (int i=0; i<records.size(); i++) {
+            int j=0;
+            for (Record r : records.get(i)) {
+                System.out.println(j + ": " + r);
+                j++;
+            }
+        }*/
     }
     
     public List<Record> sendRecords(int id)
     {
-        ArrayList<Record> toSend = new ArrayList<Record>();
-        for (ArrayList<Record> row : records)
-        {
-            for (Record record : row)
-            {
-                if (record != null)
+        if ((possibleSpies.get(id).size() > 30) || (id == spy)) {
+            return null;
+        } else {
+            ArrayList<Record> toSend = new ArrayList<Record>();
+            if (!isSpy) {
+                for (ArrayList<Record> row : records)
                 {
-                    toSend.add(record);
+                    for (Record record : row)
+                    {
+                        if (record != null)
+                        {
+                            toSend.add(record);
+                        }
+                    }
+                }
+            } else {
+                int m = 0;
+                for (ArrayList<Record> row : records) {
+                    for (Record record : row) {
+                        if (record != null) {
+                            if (m%3 == 0) {
+                                //System.out.println("WE WANT TO LIE");
+                                Record newRec = new Record(record.getLoc(), (record.getC()+1)%2, (record.getPT()+1)%3, record.getObservations());
+                                toSend.add(newRec);
+                            } else {
+                                toSend.add(record);
+                            }
+                        }
+                        m += 1;
+                    }
                 }
             }
+            return toSend;
         }
-        return toSend;
     }
     
     public void receiveRecords(int id, List<Record> records)
     {
-        // Compare their records against ours
-        /*for (Record rec_r : records) {
-            
-        }*/
+        if (id != spy) {
+            // Compare received records against those that we have in our trueRecords list
+            // If there is a conflict
+            for (Record recR : records) {
+                Point p = recR.getLoc();
+                Record ourRecord = this.trueRecords.get(p);
+                if (ourRecord != null) {
+                    if (!((ourRecord.getC() == recR.getC()) && (ourRecord.getPT() == recR.getPT()))) {
+                        //System.out.println("player " + id + " LIED!");
+                        if (!(possibleSpies.containsKey(id))) {
+                            HashSet<Point> clashPs = new HashSet<Point>();
+                            clashPs.add(p);
+                            possibleSpies.put(id, clashPs);
+                        } else {
+                            if (!(possibleSpies.get(id).contains(p))) {
+                                possibleSpies.get(id).add(p);
+                            }
+                        }
+                        //System.out.println("p" + id + " suspicion score = " + possibleSpies.get(id).size());
+                        if (recR.getObservations().size() < 2) {
+                            spy = id;
+                        }
+                        
+                    } else {
+                        //System.out.println("player " + id + " told the TRUTH!");
+                    }
+                }
+            }
+        }
+        
+
+        // If we are in the chain of observations
+        // If we are first in the chain: compare information against our own obswerved records
+        // If not first in chain: compare sequence of observations with the corresponding sequence of observations in records
         
     }
     
