c64917992289ca7816c0a021c11e9d7f86e71a03
chengxuHawkwood <2041829103@qq.com>
2018-11-25 10:50:39 -0500

    vote

--- spy/g7/Player.java
+++ spy/g7/Player.java
@@ -23,10 +23,39 @@ public class Player implements spy.sim.Player {
     private Point loc;
     private HashMap<Point, List<Record>> trustRecords;
     private HashMap<Point, List<Record>> tempRecords;
+    private PriorityQueue<Vote> lastResult;
 
-
+    class PathTime{
+        Integer id;
+        Integer time;
+        PathTime(int id, int time){
+            this.id = id;
+            this.time = time;
+        }
+        public Integer getId() {
+            return id;
+        }
+        public Integer getTime() {
+            return time;
+        }
+    }
+    class Vote{
+       Integer id;
+       Integer number;
+       Vote(int id, int number){
+           this.id = id;
+           this.number = number;
+       }
+       public Integer getId() {
+            return id;
+       }
+       public Integer getNumber() {
+            return number;
+       }
+    }
     public void init(int n, int id, int t, Point startingPos, List<Point> waterCells, boolean isSpy)
     {
+        lastResult = null;
         trust  = new HashSet<>();
         trustRecords = new HashMap<>();
         tempRecords = new HashMap<>();
@@ -44,7 +73,28 @@ public class Player implements spy.sim.Player {
         }
         player_num = n ;
     }
-    
+
+
+    private boolean pointsAreAdjacent(Point p1, Point p2)
+    {
+        return Math.abs(p1.x - p2.x) <= 1 && Math.abs(p1.y - p2.y) <= 1;
+    }
+
+    private int moveTime(Point from, Point to)
+    {
+        if (from.equals(to))
+        {
+            return 1;
+        }
+        int base = 2;
+        if (from.x != to.x && from.y != to.y)
+        {
+            base = 3;
+        }
+        boolean isMuddy = trustRecords.get(from).get(0).getC() == 1 || trustRecords.get(to).get(0).getC() == 1;
+        return isMuddy ? base * 2 : base;
+    }
+
     public void observe(Point loc, HashMap<Point, CellStatus> statuses)
     {
         this.loc = loc;
@@ -67,7 +117,23 @@ public class Player implements spy.sim.Player {
         }
 
     }
-    
+
+    private int calculateTime(List<Point> path){
+        int totalTime = 0;
+        for (int i = 1; i < path.size(); i++)
+        {
+            Point from = path.get(i - 1);
+            Point to = path.get(i);
+            if (!pointsAreAdjacent(from, to))
+                return Integer.MAX_VALUE;
+            if (!trustRecords.containsKey(from)||!trustRecords.containsKey(to))
+                return Integer.MAX_VALUE;
+            totalTime += moveTime(from, to) * 5;
+        }
+        return totalTime;
+    }
+
+
     public List<Record> sendRecords(int id)
     {
         List<Record> send = new ArrayList<Record>();
@@ -87,49 +153,59 @@ public class Player implements spy.sim.Player {
     
     public void receiveRecords(int id, List<Record> records)
     {
-        for(Record r:records){
-            List<Record> l = new ArrayList<>();
-            l.add(r);
-            tempRecords.put(r.getLoc() ,l);
-        }
-        for (Map.Entry<Point, List<Record>> entry : tempRecords.entrySet()){
-            List<Record> rl= entry.getValue();
-            for(int i =0; i<rl.size()-1;i++){
-                Set<Integer> playerInI = new HashSet<>();
-                List<Observation> obInI = rl.get(i).getObservations();
-                for(Observation o: obInI)
-                    playerInI.add(o.getID());
-                for(int j=i; j<rl.size();j++){
-                    if(rl.get(i).getC() != rl.get(j).getC() || rl.get(i).getPT() != rl.get(j).getPT()){
-                        Set<Integer> playerInJ = new HashSet<>();
-                        List<Observation> obInJ = rl.get(j).getObservations();
-                        for(Observation o: obInJ)
-                            playerInJ.add(o.getID());
-                        playerInJ.addAll(playerInI);   //If there are conflicts then, spys will not be out of the two sets
-                        Set<Integer> untrust= new HashSet();
-                        for(int k = 0; k<player_num;k++)
-                            untrust.add(k);
-                        untrust.removeAll(playerInJ); //remove all players might be spys
-                        trust.addAll(untrust);
-                    }
-                }
+        for(Record r: records){
+            if(!trustRecords.containsKey(r.getLoc())){
+                List<Record> rl = new ArrayList<>();
+                rl.add(r);
+                trustRecords.put(r.getLoc(), rl);
+            }else {
+                List<Record> rl = trustRecords.get(r.getLoc());
+                rl.add(r);
             }
         }
-        for(Record r:records){
-            List<Observation> obs= r.getObservations();
-            boolean flag = true;
-            for(Observation ob: obs){
-                if(!trust.contains(ob.getID())) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                List<Record> temp = trustRecords.get(r.getLoc());
-                temp.add(r);
-                trustRecords.put(r.getLoc(), temp);
-            }
-        }
+//        for(Record r:records){
+//            List<Record> l = new ArrayList<>();
+//            l.add(r);
+//            tempRecords.put(r.getLoc() ,l);
+//        }
+//        for (Map.Entry<Point, List<Record>> entry : tempRecords.entrySet()){
+//            List<Record> rl= entry.getValue();
+//            for(int i =0; i<rl.size()-1;i++){
+//                Set<Integer> playerInI = new HashSet<>();
+//                List<Observation> obInI = rl.get(i).getObservations();
+//                for(Observation o: obInI)
+//                    playerInI.add(o.getID());
+//                for(int j=i; j<rl.size();j++){
+//                    if(rl.get(i).getC() != rl.get(j).getC() || rl.get(i).getPT() != rl.get(j).getPT()){
+//                        Set<Integer> playerInJ = new HashSet<>();
+//                        List<Observation> obInJ = rl.get(j).getObservations();
+//                        for(Observation o: obInJ)
+//                            playerInJ.add(o.getID());
+//                        playerInJ.addAll(playerInI);   //If there are conflicts then, spys will not be out of the two sets
+//                        Set<Integer> untrust= new HashSet();
+//                        for(int k = 0; k<player_num;k++)
+//                            untrust.add(k);
+//                        untrust.removeAll(playerInJ); //remove all players might be spys
+//                        trust.addAll(untrust);
+//                    }
+//                }
+//            }
+//        }
+//        for(Record r:records){
+//            List<Observation> obs= r.getObservations();
+//            boolean flag = true;
+//            for(Observation ob: obs){
+//                if(!trust.contains(ob.getID())) {
+//                    flag = false;
+//                    break;
+//                }
+//            }
+//            if(flag){
+//                List<Record> temp = trustRecords.get(r.getLoc());
+//                temp.add(r);
+//                trustRecords.put(r.getLoc(), temp);
+//            }
+//        }
 
     }
     
@@ -137,21 +213,33 @@ public class Player implements spy.sim.Player {
     {
         return null;
     }
-    
+
     public List<Integer> getVotes(HashMap<Integer, List<Point>> paths)
     {
-        for (Map.Entry<Integer, List<Point>> entry : paths.entrySet())
-        {
-            ArrayList<Integer> toReturn = new ArrayList<Integer>();
-            toReturn.add(entry.getKey());
-            return new ArrayList<Integer>(entry.getKey());
+        PriorityQueue<PathTime> heap = new PriorityQueue<PathTime>((x,y)->(x.getTime()-y.getTime()));
+        if(lastResult!=null){
+            return new ArrayList<Integer>(lastResult.poll().getId());
+        }else{
+            for (Map.Entry<Integer, List<Point>> entry : paths.entrySet())
+            {
+                if(calculateTime(entry.getValue())!=Integer.MAX_VALUE)
+                    heap.add(new PathTime(entry.getKey(),calculateTime(entry.getValue())));
+            }
+            if(heap.size()!=0){
+                return new ArrayList<Integer>(heap.poll().getId());
+            }
         }
         return null;
     }
     
     public void receiveResults(HashMap<Integer, Integer> results)
     {
-        
+        lastResult = new PriorityQueue<Vote>((x, y) -> y.getNumber() - x.getNumber());
+        for (Map.Entry<Integer, Integer> entry : results.entrySet()) {
+           Vote v= new Vote(entry.getKey(), entry.getValue());
+           lastResult.add(v);
+        }
+
     }
     
     public Point getMove() {
