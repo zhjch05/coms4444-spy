8e5d147b481537628af7cee28fe019d8a2033df0
LastRemote <b.qu@columbia.edu>
2018-12-06 21:46:55 -0500

    Update WaitTask.java
    Wait now functions properly

--- spy/g6/WaitTask.java
+++ spy/g6/WaitTask.java
@@ -1,37 +1,25 @@
 package spy.g6;
 
-import java.util.LinkedList;
-
 import spy.sim.Point;
 
 public class WaitTask extends MovementTask {
 	// protected LinkedList<Point> moves;
 	
-
+	int roundsWait;
 	//wait at point and avoid communication
-	public WaitTask(PathFinder pathFinder, Point loc, Point packageLoc) {
+	public WaitTask(int i) {
 		super();
-		LinkedList<Point> points = pathFinder.startSearch(loc, packageLoc, false);
-		moves = new LinkedList<Point>();
-		Point prev = null;
-		for (Point p: points) {
-			if (prev != null) {
-				//System.err.print(new Point(p.x - prev.x, p.y - prev.y));
-				moves.addLast(new Point(p.x - prev.x, p.y - prev.y));
-			}
-			prev = p;
-		}
+		roundsWait = i;
 	}
 
 	public boolean isCompleted() {
-		return moves.isEmpty();
+		return (roundsWait == 0);
 		// return false;
 	}
 	
 	@Override
 	public Point nextMove(){
-		if (moves.isEmpty())
-			return new Point(0, 0);
-	 	return moves.removeFirst();
+		--roundsWait;
+		return new Point(0, 0);
 	}
 }
